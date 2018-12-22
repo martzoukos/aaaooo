cb0a416fa0e0388e804c25e19d96d8fe0d20d9c1
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2018-12-22 23:02:35 +0100

    Show only specific characters.

--- components/letter.jsx
+++ components/letter.jsx
@@ -2,24 +2,34 @@ class Letter extends React.Component {
 	 constructor(props) {
     super(props);
     this.state = {
-    	letter: "A"
+    	letter: this.props.value
     };
   }
 
   updateLetter = (event) => {
-		this.setState({
-	  	letter: event.key
-	  });
+  	console.log(event.keyCode);
+  	if (
+  		// https://keycode.info/
+  		// This seemingly random list of keys representes alphanumeric and special characters. 
+  		(event.keyCode >= 48 && event.keyCode <= 90)
+  	  || (event.keyCode >= 96 && event.keyCode <= 111)
+  	  || (event.keyCode >= 160 && event.keyCode <= 165)
+  	  || (event.keyCode >= 186 && event.keyCode <= 223))
+  	{
+			this.setState({
+		  	letter: event.key
+		  });	
+  	}
 	}
 
 	render() {
 		return (
-			<div>
-				<div className="the-letter" onKeyDown={this.updateLetter} tabIndex="0">
+			<div className="letter">
+				<div className="letter__character" onKeyDown={this.updateLetter} tabIndex="0">
 		  		{this.state.letter}
 		  	</div>
 		  	<style jsx>{`
-		  		.the-letter {
+		  		.letter__character {
 		  			background-color: #edff8f;
 		  			color: #ff5994;
 		  			font-family: "Comic Sans MS", cursive, sans-serif;
