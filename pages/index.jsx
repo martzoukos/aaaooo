23a1b38f8c718b39bf1ba3fb5cb5ab875ddefc7e
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2018-12-25 23:12:29 +0100

    Added footnote.

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,10 +28,29 @@ class Page extends React.Component {
 						onChange={this.toggleAudio}  />
 			  	<label htmlFor="toggle-audio">Play audio</label>
 			  </div>
+			  <footer className="footer">
+			  	Created by <a href="https://twitter.com/martzoukos">@martzoukos</a>
+			  </footer>
 			  <style jsx>{`
 			  	.container {
 		  			font-family: "Comic Sans MS", cursive, sans-serif;
 		  			color: #ff5994;
+			  	}
+
+			  	a:link,
+			  	a:visited {
+			  		color: #ff5994;
+			  	}
+			  	a:hover,
+			  	a:focus {
+			  		background-color: #ff5994;
+			  		color: #edff8f;
+			  	}
+
+			  	.footer {
+			  		position: absolute;
+			  		bottom: 1em;
+			  		right: 1em;
 			  	}
 
 					.audio-control {
